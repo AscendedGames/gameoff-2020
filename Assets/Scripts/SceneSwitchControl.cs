200b9f0a0ce11494c70cf27818e2c82dc09dc3d5
Devon Kubacki <devon.kubacki@gmail.com>
2020-11-05 22:34:45 -0500

    Implemented a Basic Main Menu

---
+++ Assets/Scripts/SceneSwitchControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BtnNewGame()
+    {
+        SceneManager.LoadScene("Prototyping Scene");
+    }
+
+    public void BtnQuitGame()
+    {
+        Debug.Log("It quit.?");
+        Application.Quit();
+    }
+}
