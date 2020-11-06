5edd6ab6c80edad9e099761f727da392b030eef2
Luna Lamb <lunamarielamb@gmail.com>
2020-11-05 22:22:00 -0500

    Added vision capability

---
+++ Assets/Scripts/VisionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionController : MonoBehaviour
+{
+    public GameObject PlayerMouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.Equals(PlayerMouse) && PlayerMouse.GetComponent<HidingController>().isMouseHidden == false)
+        {
+            Debug.Log("DETECTED!");
+        }
+    }
+}
