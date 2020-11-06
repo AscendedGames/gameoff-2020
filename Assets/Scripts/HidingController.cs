18e462de6fd1e19305ac41492f71a77cae842dec
Luna Lamb <lunamarielamb@gmail.com>
2020-11-05 21:22:09 -0500

    Mouse can now hide
    Added colored text to top right to indicate when hidden

---
+++ Assets/Scripts/HidingController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HidingController : MonoBehaviour
+{
+    public Text HiddenText;
+
+    [HideInInspector]
+    public bool isMouseHidden = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("HidingArea"))
+        {
+            isMouseHidden = true;
+            HiddenText.color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name.Equals("HidingArea"))
+        {
+            isMouseHidden = false;
+            HiddenText.color = Color.red;
+        }
+    }
+}
