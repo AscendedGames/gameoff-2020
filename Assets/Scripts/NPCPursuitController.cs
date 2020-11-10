e874c68ec2295f43dba95bc1b6d4ee6ffc756727
Luna Lamb <lunamarielamb@gmail.com>
2020-11-09 20:59:44 -0500

    Fixed NPC turning bug

--- Assets/Scripts/NPCPursuitController.cs
+++ Assets/Scripts/NPCPursuitController.cs
@@ -49,7 +49,7 @@ public class NPCPursuitController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Mouse")
+        if (collision.collider.tag == "Player")
         {
             if (!_isPlayerInRange)
             {
