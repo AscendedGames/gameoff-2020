c661a174534795f4d90cff73aa4360e07a80f9ba
Devon Kubacki <devon.kubacki@gmail.com>
2020-11-09 23:45:14 -0500

    Implemented most of the Pause Menu
    Minor known bug - Quality selector doesnt show setting properly, doesn't appear to work.
    
    Minor known bug - Pause menu sub-panels appear when you pause, then scrolls off screen. This bug was implemented when pause menu showed Last screen closed when you press escape again.
    
    Added footstep pitch/volume variation code for the player.
    
    Added Pause/resume functionality
    
    Added "Are you sure you want to quit?" panel in pause menu
    
    Added "Are you sure you want to reset?" panel in pause menu

--- Assets/Scripts/SceneSwitchControl.cs
+++ Assets/Scripts/SceneSwitchControl.cs
@@ -9,9 +9,108 @@ public class SceneSwitchControl : MonoBehaviour
     private GameObject MainMenuPanel;
     private GameObject OptionsPanel;
     private GameObject CreditsPanel;
+
+    public static bool GameIsPaused = false;
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject pauseOptions;
+    [SerializeField] private GameObject quitConfirmMenu;
+    [SerializeField] private GameObject resetConfirmMenu;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+               BtnResume();
+            } else
+            {
+                BtnPause();
+            }
+        }
+    }
+
     public void BtnNewGame() //Load the Game
     {
         SceneManager.LoadScene("Prototyping Scene");
+        Time.timeScale = 1f;
+    }
+
+    public void BtnResume()
+    {
+        pauseOptions.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        quitConfirmMenu.SetActive(false);
+        resetConfirmMenu.SetActive(false);
+
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void BtnPause()
+    {
+        pauseOptions.SetActive(true);
+        pauseMenuUI.SetActive(true);
+        quitConfirmMenu.SetActive(true);
+        resetConfirmMenu.SetActive(true);
+
+        pauseMenuUI = GameObject.Find("PauseMenu");
+        pauseOptions = GameObject.Find("PauseOptions");
+        pauseMenuUI.GetComponent<Animator>().Play("Slide-Left-In");
+        pauseOptions.GetComponent<Animator>().Play("Slide-Left-Out");
+        resetConfirmMenu.GetComponent<Animator>().Play("Slide-Left-Out");
+
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void BtnPauseMenu()
+    {
+        pauseMenuUI = GameObject.Find("PauseMenu");
+        pauseOptions = GameObject.Find("PauseOptions");
+        quitConfirmMenu = GameObject.Find("QuitConfirm");
+        resetConfirmMenu = GameObject.Find("ResetConfirm");
+
+        pauseMenuUI.GetComponent<Animator>().Play("Slide-Left-In");
+        pauseOptions.GetComponent<Animator>().Play("Slide-Left-Out");
+        quitConfirmMenu.GetComponent<Animator>().Play("Slide-Left-Out");
+        resetConfirmMenu.GetComponent<Animator>().Play("Slide-Left-Out");
+    }
+
+    public void BtnOptionsMenu()
+    {
+        pauseMenuUI = GameObject.Find("PauseMenu");
+        pauseOptions = GameObject.Find("PauseOptions");
+
+        pauseMenuUI.GetComponent<Animator>().Play("Slide-Right-Out");
+        pauseOptions.GetComponent<Animator>().Play("Slide-Right-In");
+    }
+
+    public void BtnQuitConfirm()
+    {
+        pauseMenuUI = GameObject.Find("PauseMenu");
+        pauseOptions = GameObject.Find("PauseOptions");
+        quitConfirmMenu = GameObject.Find("QuitConfirm");
+
+        pauseMenuUI.GetComponent<Animator>().Play("Slide-Left-Out");
+        pauseOptions.GetComponent<Animator>().Play("Slide-Left-Out");
+        quitConfirmMenu.GetComponent<Animator>().Play("Slide-Left-In");
+    }
+
+    public void BtnResetConfirm()
+    {
+        pauseMenuUI = GameObject.Find("PauseMenu");
+        pauseOptions = GameObject.Find("PauseOptions");
+        resetConfirmMenu = GameObject.Find("ResetConfirm");
+
+        pauseMenuUI.GetComponent<Animator>().Play("Slide-Left-Out");
+        pauseOptions.GetComponent<Animator>().Play("Slide-Left-Out");
+        resetConfirmMenu.GetComponent<Animator>().Play("Slide-Left-In");
     }
 
     public void BtnMainMenu() //Load the Main Menu
@@ -19,6 +118,7 @@ public class SceneSwitchControl : MonoBehaviour
         OptionsPanel = GameObject.Find("OptionsPanel");
         MainMenuPanel = GameObject.Find("MainMenuPanel");
         CreditsPanel = GameObject.Find("CreditsPanel");
+
         OptionsPanel.GetComponent<Animator>().Play("Slide-Right-Out");
         CreditsPanel.GetComponent<Animator>().Play("Slide-Right-Out");
         MainMenuPanel.GetComponent<Animator>().Play("Slide-Right-In");
@@ -28,6 +128,7 @@ public class SceneSwitchControl : MonoBehaviour
     {
         OptionsPanel = GameObject.Find("OptionsPanel");
         MainMenuPanel = GameObject.Find("MainMenuPanel");
+
         OptionsPanel.GetComponent<Animator>().Play("Slide-Left-In");
         MainMenuPanel.GetComponent<Animator>().Play("Slide-Left-Out");
     }
@@ -36,6 +137,7 @@ public class SceneSwitchControl : MonoBehaviour
     {
         CreditsPanel = GameObject.Find("CreditsPanel");
         MainMenuPanel = GameObject.Find("MainMenuPanel");
+
         CreditsPanel.GetComponent<Animator>().Play("Slide-Left-In");
         MainMenuPanel.GetComponent<Animator>().Play("Slide-Left-Out");
     }
@@ -44,4 +146,5 @@ public class SceneSwitchControl : MonoBehaviour
     {
         Application.Quit();
     }
+
 }
