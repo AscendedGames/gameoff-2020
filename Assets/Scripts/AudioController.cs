c5b4e84aefc427cc7ec4b00462634cfdfb14ce68
Devon Kubacki <devon.kubacki@gmail.com>
2020-11-13 19:25:42 -0500

    Commit-Hold

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private void Update ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = Random.Range(1.7f, 3.0f);
+        audioSource.volume = Random.Range(0.358f, 0.420f);
+    }
+}
