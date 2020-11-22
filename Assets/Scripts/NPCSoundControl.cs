96257c8029387337d07575e1bf6c6f13ecf0a26b
Luna Lamb <lunamarielamb@gmail.com>
2020-11-22 17:01:35 -0500

    Fixed stuff

--- Assets/Scripts/NPCSoundControl.cs
+++ Assets/Scripts/NPCSoundControl.cs
@@ -7,16 +7,18 @@ public class NPCSoundControl : MonoBehaviour
     [SerializeField]
     private AudioSource walkingSounds;
     [SerializeField]
-    private float stepSoundDelay; //To define the stepDelay inside of Unity
+    private float stepSoundDelay = 0.5f; // To define the stepDelay inside of Unity
     [SerializeField]
     private Animator NPCAnimation;
 
     private bool couroutineOn; // For Adding the Coroutine to delay the walking sounds
+    private NPCPursuitController _npcPursuitController;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<NPCPursuitController>();
+        _npcPursuitController = GetComponent<NPCPursuitController>();
+        NPCAnimation = GetComponent<Animator>();
         couroutineOn = true;
         StartCoroutine(Patroling());
     }
@@ -26,23 +28,23 @@ public class NPCSoundControl : MonoBehaviour
 
         while (couroutineOn == true)
         {
-            if (GetComponent<NPCPursuitController>().IsInPursuit == false && GetComponent<NPCPursuitController>().HasBrokenPursuit == false)
+            if (_npcPursuitController.IsInPursuit == false && _npcPursuitController.HasBrokenPursuit == false)
             {
                 walkingSounds.enabled = true;
-                NPCAnimation.GetComponent<Animator>().Play("Scientist Walk");
+                NPCAnimation.Play("Scientist Walk");
                 walkingSounds.pitch = (Random.Range(1f, 1.1f));
                 walkingSounds.Play();
             }
-            else if (GetComponent<NPCPursuitController>().IsInPursuit == true)
+            else if (_npcPursuitController.IsInPursuit == true)
             {
                 walkingSounds.enabled = true;
-                NPCAnimation.GetComponent<Animator>().Play("Scientist Walk");
+                NPCAnimation.Play("Scientist Walk");
                 walkingSounds.pitch = (Random.Range(1.3f, 1.5f));
                 walkingSounds.Play();
             }
-            else if (GetComponent<NPCPursuitController>().HasBrokenPursuit == true)
+            else if (_npcPursuitController.HasBrokenPursuit == true)
             {
-                NPCAnimation.GetComponent<Animator>().Play("Scientist-Idle");
+                NPCAnimation.Play("Scientist-Idle");
                 walkingSounds.enabled = false;
             }
 
