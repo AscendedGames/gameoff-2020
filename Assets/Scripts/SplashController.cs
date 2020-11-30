8531ee012b32bc9be66b6672a9ebb097b25ce16e
Luna Lamb <lunamarielamb@gmail.com>
2020-11-30 15:49:53 -0500

    Added splash screen

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    public float delayTime = 3.0f;
+    private LevelLoader levelLoader;
+
+    void Start()
+    {
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+
+        GoToMainMenu();
+    }
+
+    void GoToMainMenu()
+    {
+        StartCoroutine(TransitionToMainMenu());
+    }
+
+    IEnumerator TransitionToMainMenu()
+    {
+
+        yield return new WaitForSeconds(delayTime);
+
+        levelLoader.TransitionToLevel("MainMenu");
+    }
+}
